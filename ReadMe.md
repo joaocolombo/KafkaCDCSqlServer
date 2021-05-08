f29032a783e612c29acaf6941d7eba444e5d2224
JOAO RAFAEL COLOMBO <joao.colombo-ext@viavarejo.com.br>
2021-05-08 18:08:07 -0300

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# pw SQL Cwi123456@
+# docker-compose up --force-recreate  -d
+#Apos container up rodar comando ou fazer chamada no postman
+curl --location --request POST 'http://0.0.0.0:8083/connectors' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "name": "inventory-connector", 
+  "config": {
+    "connector.class": "io.debezium.connector.sqlserver.SqlServerConnector", 
+    "database.hostname": "sqlserver", 
+    "database.port": "1433", 
+    "database.user": "sa", 
+    "database.password": "Cwi123456@", 
+    "database.dbname": "Origem", 
+    "database.server.name": "origem", 
+    "table.include.list": "dbo.Lojista,dbo.sku,dbo.skulojista,dbo.produto", 
+    "database.history.kafka.bootstrap.servers": "kafka:9092", 
+    "database.history.kafka.topic": "dbhistory.origem" 
+  }
+}'
---
+++ kafkaConsumer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+
+        var taskSkuLojista = new ConsumerTopico("origem.dbo.SkuLojista");
+        var taskLojista = new ConsumerTopico("origem.dbo.Lojista");
+        var taskSku = new ConsumerTopico("origem.dbo.Produto");
+        var taskProduto = new ConsumerTopico("origem.dbo.Sku");
+
+        await Task.WhenAll(new[] {taskProduto.Run(), taskLojista.Run(), taskSku.Run(), taskSkuLojista.Run()});
+    }
+
+
+    public class ConsumerTopico
+    {
+        public ConsumerTopico(string topico)
+        {
+            Topico = topico;
+        }
+
+        private string Topico { get; }
+        public async Task Run()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "consumer1",
+                BootstrapServers = "kafka",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+            c.Subscribe(Topico);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        await Task.Delay(10, cts.Token);
+                        var cr = c.Consume(cts.Token);
+                        Console.WriteLine($"------------------------{Topico}-----------------------------");
+
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException e)
+            {
+                c.Close();
+            }
+
+        }
+    }
+}
+
